13de4ccab0bd69ee8cc8bfb215442ab2794cd350
Asus <Asus@satish>
2024-05-12 14:20:14 +0530

    By Satish

--- GitDemoProject/src/Day1/program1.java
+++
@@ -1,9 +0,0 @@
-package Day1;
-
-public class program1 {
-	public static void main(String[] args)
-	{
-		System.out.println("This is Git demo first project.");
-	}
-
-}
--- GitProject/src/module-info.java
+++ GitProject/src/module-info.java
@@ -4,5 +4,5 @@
 /**
  * 
  */
-module GitDemoProject {
+module GitProject {
 }
