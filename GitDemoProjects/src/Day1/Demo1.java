e1fed917f4c001d5b4a851dd5a482a19c5e5597e
Gaikwad-Satish <satishg02018@gmail.com>
2024-05-13 15:11:47 +0530

    Update Demo1.java

--- GitDemoProjects/src/Day1/Demo1.java
+++ GitDemoProjects/src/Day1/Demo1.java
@@ -1,9 +1,18 @@
 package Day1;
 
 public class Demo1 {
+	public static void add()
+	{
+		int a=20;
+		int b=50;
+		System.out.println(a+b);
+		
+	}
+	
 	public static void main(String[] args)
 	{
 		System.out.println("Hello.This is first Git Project.");
 	}
+	
 
 }
