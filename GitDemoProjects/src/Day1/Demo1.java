8f00f2a2f9bde5f50a51e6c6d6945ce66d7e078d
Asus <Asus@satish>
2024-05-13 15:25:06 +0530

    Demo1 file modified

--- GitDemoProjects/src/Day1/Demo1.java
+++ GitDemoProjects/src/Day1/Demo1.java
@@ -9,6 +9,11 @@ public class Demo1 {
 		
 	}
 	
+	public static void mul()
+	{
+		System.out.println(5*2);
+	}
+	
 	public static void main(String[] args)
 	{
 		System.out.println("Hello.This is first Git Project.");
